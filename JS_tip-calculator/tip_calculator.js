0a2ec3b04ad983a4a1e56634bbdc826fa1f5673c
Ren <renr@wit.edu>
2020-05-02 15:35:10 -0400

    added events to minus and plus buttons

--- JS_tip-calculator/tip_calculator.js
+++ JS_tip-calculator/tip_calculator.js
@@ -1,5 +1,8 @@
 let totalValue;
 
+let partyCount = document.getElementById("count");
+let i = 1; //holds the current value of Split_Check
+
 var total = document.getElementById("total");
 
 total.addEventListener("input", () => {
@@ -11,11 +14,27 @@ let service = document.getElementById("service");
 let calculateTip = document.getElementById("calculateTip");
 
 calculateTip.addEventListener("click", () => {
-    let billTotal = parseFloat(totalValue) * parseFloat(service.value) + parseFloat(totalValue);
-
+    let billTotal =  ((parseFloat(totalValue) * parseFloat(service.value) + parseFloat(totalValue)) / i) ;
+    
     let display = document.getElementById("display");
-    display.innerHTML = billTotal;
+    display.innerHTML = "$" + parseFloat(billTotal.toFixed(2));
 })
 
 
+
+let increaseButton = document.getElementById("plus");
+let decreaseButton = document.getElementById("minus");
+
+increaseButton.addEventListener("click", function() {
+    ++i;
+    partyCount.innerHTML = i;
+    console.log(i);
+})
+
+decreaseButton.addEventListener("click", function() {
+    if(i !== 1) {
+        partyCount.innerHTML = --i;
+    }
+    console.log(i);
+})
 
